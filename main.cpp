d68582d0b61ddb14ea970e2e2bb33b2538910972
sit3kk <konrad.sitek@swmansion.com>
2025-01-22 18:35:52 +0100

    Moved from another repo

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <boost/filesystem/operations.hpp>
-#include <iostream>
-
-int main(int argc, char *argv[]) {
-    std::cout << "The size of " << boost::filesystem::absolute(argv[0])
-              << " is " << boost::filesystem::file_size(argv[0]) << '\n';
-    return 0;
-}
